e38ce70ec1947107b7da24eadc46e81c05ffde76
Karol <bukowski.kar@gmail.com>
2024-10-29 17:05:41 +0100

    Zejsciowka_2_v1

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,35 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    ResourceManager() : resource{new Resource{}} {}
+
+    ResourceManager(const ResourceManager& rm) : resource{new Resource{*rm.resource}} {}
+    ResourceManager(ResourceManager&& rm) noexcept : resource(rm.resource)
+    {
+        rm.resource = nullptr;
+    }
+    ResourceManager& operator=(const ResourceManager& rm)
+    {
+        if (this != &rm) {
+            delete resource;
+            resource = new Resource(*rm.resource);
+        }
+        return *this;
+    }
+    ResourceManager& operator=(ResourceManager&& rm) noexcept
+    {
+        if (this != &rm) {
+            delete resource;
+            resource    = rm.resource;
+            rm.resource = nullptr;
+        }
+        return *this;
+    }
+    ~ResourceManager() { delete resource; }
+
+    double get() const { return resource->get(); }
+
+private:
+    Resource* resource;
 };
